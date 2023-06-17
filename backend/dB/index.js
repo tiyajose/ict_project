bbeb52f37946b849b0a0706bb1091b0fdb145009
Anuroopa K S <83598585+Anuroopaaa@users.noreply.github.com>
2023-06-17 10:53:37 +0530

    Update psw index.js

--- backend/dB/index.js
+++ backend/dB/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://tiyajo2002:Pass123456789@cluster0.oft3egy.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://tiyajo2002:< >@cluster0.oft3egy.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDb Atlas connection established')
     })
