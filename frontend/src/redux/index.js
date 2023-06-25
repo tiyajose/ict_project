d837567eec2bf8f1234320f91b39cc0c10c93d3b
anuroopa <anuroopa.saji@gmail.com>
2023-06-25 10:54:31 +0530

    final

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./userSlice";
-import productSlideReducer from "./productSlide";
+import productSlideReducer from "./productSlice";
 
 export const store = configureStore({
   reducer: {
